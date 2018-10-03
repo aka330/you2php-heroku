802c3b23f9d25c8fc208d8beb63712ced1cafec6
aka330 <43809231+aka330@users.noreply.github.com>
2018-10-04 07:08:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ define('APIKEY', getenv("AIzaSyCPNjIxoiF9TCrMlaA9Cq90ttMgYxFKjuU"));
 define('GJ_CODE', getenv("JP"));
 define('SITE_NAME',getenv("ようこそ"));
 define('TITLENAME', getenv("ようこそ"));
-define('EN2DEKEY',getenv("68NhSBwAcLK"));
+define('EN2DEKEY',getenv("68NhSB9044wAcLK"));
 define('EMAIL', getenv("eruka32@yahoo.co.jp"));
 ?>
